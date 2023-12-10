099864db8ffbcd5fb91559c1a6636246b4991c5c
nsd6803 <nsd6803@mail.ru>
2023-12-10 22:36:52 +0300

    first build success

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO armor (id, name, icon, picture, type, predicat, exoticicon, perkname, perkdesc, lore) VALUES
+                                         (1, 'name', 'icon', 'picture', 'type', 'predicat', 'exoticicon', 'perkname', 'perkdesc', 'lore');
