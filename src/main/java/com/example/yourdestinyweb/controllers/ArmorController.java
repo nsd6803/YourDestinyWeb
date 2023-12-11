90b15ae412725529561cbd627c0cb4d42d6c3942
nsd6803 <nsd6803@mail.ru>
2023-12-11 03:19:11 +0300

    first build success

--- src/main/java/com/example/yourdestinyweb/controllers/ArmorController.java
+++ src/main/java/com/example/yourdestinyweb/controllers/ArmorController.java
@@ -2,11 +2,11 @@ package com.example.yourdestinyweb.controllers;
 
 import com.example.yourdestinyweb.models.Armor;
 import com.example.yourdestinyweb.services.ArmorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,4 +24,47 @@ public class ArmorController {
         return armourService.allArmour();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Armor> armour(Model model, @PathVariable Long id) {
+        Armor armor = armourService.find_Armour(id).getBody();
+
+        if (armor != null) {
+            return ResponseEntity.ok(armor);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<Armor> add_armour(@RequestBody Armor armor) {
+        try {
+            armourService.saveArmor(armor);
+            return ResponseEntity.ok(armor);
+        } catch (Exception e) {
+            e.printStackTrace();  // Log the exception details
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteArmor(@PathVariable Long id) {
+        try {
+            armourService.deleteArmorById(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            e.printStackTrace();  // Log the exception details
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PutMapping("/change/{id}")
+    public ResponseEntity<Void> updateArmor(@PathVariable Long id, @RequestBody Armor updatedArmor) {
+        try {
+            armourService.updateArmor(id, updatedArmor);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            e.printStackTrace();  // Log the exception details
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
--- src/main/java/com/example/yourdestinyweb/services/ArmorService.java
+++ src/main/java/com/example/yourdestinyweb/services/ArmorService.java
@@ -4,9 +4,11 @@ import com.example.yourdestinyweb.models.Armor;
 import com.example.yourdestinyweb.repositories.ArmorRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ArmorService {
@@ -19,5 +21,40 @@ public class ArmorService {
 
     public List<Armor> allArmour() {
         return armorRepository.findAll();
+    }
+
+    public ResponseEntity<Armor> find_Armour(Long id) {
+        Optional<Armor> optionalArmor = armorRepository.findById(id);
+
+        return optionalArmor.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    public void saveArmor(Armor armor) {
+        armorRepository.save(armor);
+    }
+
+    public void deleteArmorById(Long id) {
+        armorRepository.deleteById(id);
+    }
+
+    public void updateArmor(Long id, Armor updatedArmor) {
+        // Retrieve the existing armor
+        Armor existingArmor = armorRepository.findById(id).orElseThrow(() -> new RuntimeException("Armor not found"));
+
+        // Update the fields with the new values
+        existingArmor.setName(updatedArmor.getName());
+        existingArmor.setIcon(updatedArmor.getIcon());
+        existingArmor.setPicture(updatedArmor.getPicture());
+        existingArmor.setType(updatedArmor.getType());
+        existingArmor.setPredicat(updatedArmor.getPredicat());
+        existingArmor.setExotic_icon(updatedArmor.getExotic_icon());
+        existingArmor.setPerk_name(updatedArmor.getPerk_name());
+        existingArmor.setPerk_desc(updatedArmor.getPerk_desc());
+        existingArmor.setFor_class(updatedArmor.getFor_class());
+        existingArmor.setLore(updatedArmor.getLore());
+
+        // Save the updated armor
+        armorRepository.save(existingArmor);
     }
 }
